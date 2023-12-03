aca39f9dea6266aa59a9e022579ccd4845f556f5
matgoms <mat.goms@gmail.com>
2023-12-03 13:40:21 -0300

    new site

--- docs/tailwind/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Menu from "./Menu";
-import Content from "./Content";
-import Footer from "./Footer";
-
-
-ReactDOM.render(<><Menu /><Content /><Footer /></>, document.getElementById("root"));
